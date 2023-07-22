743d9eaaf3c7cd3957e4319f55e14de7ba3c3aef
Daniel Berenfield <dberenfield@gmail.com>
2023-07-22 17:20:12 -0400

    changes for hw

--- public/index.js
+++ public/index.js
@@ -1,9 +1,60 @@
+
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    async function fetchData(){
+    const url = 'https://api.twelvedata.com/time_series?symbol=AAPL&interval=0.99min&apikey=4cc5ffb41dbd4c11bf41de7e77799433'
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    return data;
+    }
+
+    const { GME, MSFT, DIS, BNTX } = mockData;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }
+    
+
+   stocks.forEach( stock => stock.values.reverse())
+
+// Time Chart
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.reverse().map(value => value.datetime),
+        datasets: stocks.map(stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.reverse().map(value => parseFloat(value.high)),
+            backgroundColor: getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
+
+    
+
 }
 
+
+
 main()
+
